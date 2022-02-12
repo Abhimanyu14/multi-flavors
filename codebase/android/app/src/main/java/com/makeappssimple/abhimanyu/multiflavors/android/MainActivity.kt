f30d8f7acbe0f1ecab18b6275097c7a70bd3a747
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-02-12 15:40:40 +0530

    created new project

---
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/multiflavors/android/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.makeappssimple.abhimanyu.multiflavors.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.makeappssimple.abhimanyu.multiflavors.android.ui.theme.MultiFlavorsTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MultiFlavorsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = "Hello Android!",
+        )
+    }
+}
